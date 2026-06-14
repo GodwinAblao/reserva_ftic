018f9a21fefb0387f8eafdf5df6508f80ccd5a81
DallleHurst <202310147@fit.edu.ph>
2026-06-15 01:06:52 +0800

    fix: remove CSS import from assets/app.js to fix Railway deployment issue

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,4 @@
 import './stimulus_bootstrap.js';
-import './styles/app.css';
 
 /* â”€â”€ Global: Reservation detail modal (used by both admin & user sidebars) â”€â”€ */
 window.showResvDetail = function(r) {
